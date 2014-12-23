c759cfde503e17b7286b4347fa2156c44b6d0664
usbpaul <usb.paul@gmail.com>
2014-12-23 16:44:02 +0100

    code

--- src/main/java/nl/ambrero/spikes/HelloWorld.java
+++ src/main/java/nl/ambrero/spikes/HelloWorld.java
@@ -2,10 +2,7 @@ package nl.ambrero.spikes;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        sayHello();
-    }
-
-    private static void sayHello() {
         System.out.printf("Hello, World");
     }
+
 }
